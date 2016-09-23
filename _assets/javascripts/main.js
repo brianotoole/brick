7e0a999d5385f05e41f3a9484deb2416cc765827
brianotoole <botoole518@gmail.com>
2016-09-23 12:10:05 -0400

    added smooth scroll to hero

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -3,3 +3,4 @@
 
 //Site
 #= require form-steps
+#= require scrolling
---
+++ _assets/javascripts/scrolling.js
@@ -0,0 +1,20 @@
+/*----------------------------------------------------*/
+/* Smooth Scrolling
+------------------------------------------------------ */
+
+jQuery(document).ready(function($) {
+
+   $('.scroll-it').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash,
+	    $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 800, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+
+});
